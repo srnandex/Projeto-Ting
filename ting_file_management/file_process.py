cad75c49221b2b088e80aa0f3a167098aeabb772
Fernando Rocha <fernandodasilvarocha2@gmail.com>
2023-02-12 01:50:48 -0300

    req 5

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,4 +1,5 @@
 from ting_file_management.file_management import txt_importer
+import sys
 
 
 def process(path_file, instance):
@@ -26,4 +27,7 @@ def remove(instance):
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        print(instance.search(position))
+    except IndexError:
+        print("Posição inválida", file=sys.stderr)
