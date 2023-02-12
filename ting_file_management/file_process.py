4bc6a152ccfc442a62e7eaa727e365227aefff8a
Fernando Rocha <fernandodasilvarocha2@gmail.com>
2023-02-12 01:31:50 -0300

    req 4

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -18,7 +18,11 @@ def process(path_file, instance):
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    try:
+        result = instance.dequeue()["nome_do_arquivo"]
+        print(f"Arquivo {result} removido com sucesso")
+    except IndexError:
+        print("Não há elementos")
 
 
 def file_metadata(instance, position):
